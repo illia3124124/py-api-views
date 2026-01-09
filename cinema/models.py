29aa0072b6976e6c67d8578d4e2cfce3649c04cd
illa-j <iliacdmn@gmail.com>
2026-01-09 18:54:29 +0200

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -1,10 +1,40 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    class Meta:
+        unique_together = ("first_name", "last_name")
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
+    actors = models.ManyToManyField(Actor, related_name="movies")
+    genres = models.ManyToManyField(Genre, related_name="movies")
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name_plural = "Movies"
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.name} (rows: {self.rows}, seats in row: {self.seats_in_row})"
